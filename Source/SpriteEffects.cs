8ffcd5963d94dff2b43fac7f20181d7417ac1b45
unknown <dmanning23@hotmail.com>
2015-07-11 19:58:40 -0500

    fixed inclaudes

--- Source/SpriteEffects.cs
+++ Source/SpriteEffects.cs
@@ -1,10 +1,8 @@
-using System;
-using System.IO;
 using Microsoft.Xna.Framework;
-using Microsoft.Xna.Framework.Content;
 using Microsoft.Xna.Framework.Graphics;
 using Microsoft.Xna.Framework.Input;
 using ResolutionBuddy;
+using System;
 
 namespace SpriteEffects
 {
